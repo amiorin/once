f0c1cac90f1956b5bf5876a1b7f88cc687646fb8
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-04-04 09:19:43 +0200

    refactor: use read-bc-pars from big-config library
    - Update big-config to SHA 7876edc
    - Remove local read-bc-vars in favor of workflow/read-bc-pars

--- src/clj/io/github/amiorin/once/params.clj
+++ src/clj/io/github/amiorin/once/params.clj
@@ -6,26 +6,8 @@
    [big-config.utils :refer [debug]]
    [big-config.workflow :as workflow]
    [cheshire.core :as json]
-   [clojure.string :as str]
    [com.rpl.specter :as s]
    [io.github.amiorin.once.options :as options]))
-
-(def prefix "BC_PAR_")
-
-(defn- read-bc-vars [opts]
-  (let [params-from-env (->> (System/getenv)
-                             (filter #(str/starts-with? % prefix))
-                             (map (fn [[k v]] [(-> k
-                                                   (subs (count prefix))
-                                                   str/lower-case
-                                                   (str/replace "_" "-")
-                                                   (str/replace "." "-")
-                                                   keyword) v]))
-                             (into {}))]
-    (merge-with merge opts {::workflow/params params-from-env})))
-
-(comment
-  (read-bc-vars {::workflow/params {:foo :bar}}))
 
 (defn tofu-params
   [opts]
@@ -58,7 +40,7 @@
         tofu-smtp-params))
   (-> tap-values))
 
-(def opts-fn (comp tofu-params tofu-smtp-params read-bc-vars))
+(def opts-fn (comp tofu-params tofu-smtp-params workflow/read-bc-pars))
 
 (comment
   (debug tap-values
